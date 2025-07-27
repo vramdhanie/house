88d3f1933aa059e91297b0c6e0e875123d7a8c72
Vincent Ramdhanie <vramdhanie@gmail.com>
2025-07-27 14:41:52 -0700

    Retry the deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 
-
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="text-center">
-       The house
+       The house as we know it
       </div>
     </div>
   );
