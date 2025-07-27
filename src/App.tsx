436ad8718fd3c12c4c069a144921446d56c58faa
Vincent Ramdhanie <vramdhanie@gmail.com>
2025-07-27 14:48:34 -0700

    Tes the action again

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="text-center">
-       The house as we know it
+       The house as it is
       </div>
     </div>
   );
